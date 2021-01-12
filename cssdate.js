ee8917458276bbdd5283c1faa980e3870ff6f506
Bryce Wray <bwray@wraytx.com>
2021-01-12 14:28:13 +0000

    Tweak

--- cssdate.js
+++ cssdate.js
@@ -5,6 +5,7 @@ const md5 = require('md5')
 const globAll = require('glob-all')
 const DATAFILE = '_data/csshash.json'
 const PCSSFILE = 'csshash'
+const YEARFILE = '_data/year.json'
 cssFiles = globAll.sync([
   'src/assets/css/*.css'
 ])
@@ -27,3 +28,9 @@ var txtValue = `index-${cssMd5Total}.css`
 fs.writeFileSync(PCSSFILE, txtValue)
 // ...the latter because, otherwise, you get the following error:
 // The "data" argument must be of type string or an instance of Buffer, TypedArray, or DataView.
+
+// Year-related stuff is for when site is on .njk templating (unnecessary if on .11ty.js templating)
+var yearValue = `{
+  "copyrightYear": "${new Date().getFullYear()}"
+}`
+fs.writeFileSync(YEARFILE, yearValue)
