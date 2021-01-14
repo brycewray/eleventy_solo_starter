49aa2bec259b9a617e5bb7d03eaa0916b351a62a
Bryce Wray <bwray@wraytx.com>
2021-01-14 13:21:59 +0000

    Removing stuff unneeded in .11ty.js-based repo

--- cssdate.js
+++ cssdate.js
@@ -5,7 +5,7 @@ const md5 = require('md5')
 const globAll = require('glob-all')
 const DATAFILE = '_data/csshash.json'
 const PCSSFILE = 'csshash'
-const YEARFILE = '_data/year.json'
+// const YEARFILE = '_data/year.json'
 cssFiles = globAll.sync([
   'src/assets/css/*.css'
 ])
@@ -29,8 +29,11 @@ fs.writeFileSync(PCSSFILE, txtValue)
 // ...the latter because, otherwise, you get the following error:
 // The "data" argument must be of type string or an instance of Buffer, TypedArray, or DataView.
 
-// Year-related stuff is for when site is on .njk templating (unnecessary if on .11ty.js templating)
+// -----------------
+// Year-related stuff, below, is for when site is on .njk templating (unnecessary if on .11ty.js templating).
+/*
 var yearValue = `{
   "copyrightYear": "${new Date().getFullYear()}"
 }`
 fs.writeFileSync(YEARFILE, yearValue)
+*/
