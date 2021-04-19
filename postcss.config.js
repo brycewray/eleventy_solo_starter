d597d8bb356c0e5d2dcdd3cf5d4f1ab966c9b708
Bryce Wray <bwray@wraytx.com>
2021-04-19 08:07:33 -0500

    Switch from cssnano to postcss-csso

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,10 @@
-const cssnano = require('cssnano')({
-  preset: 'default',
-})
-
 module.exports = {
   plugins: [
-    require('postcss-nesting'),
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
-    ...process.env.NODE_ENV === 'production'
-      ? [cssnano]
-      : []
+    require('postcss-csso')({
+      restructure: true
+    }),
   ],
 }
