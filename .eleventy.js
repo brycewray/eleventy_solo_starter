5c58793bfb07166eb3698a31d3afbe42e0d23903
Bryce Wray <bwray@wraytx.com>
2021-07-13 08:18:51 -0500

    Correction due to Eleventy docs correction

--- .eleventy.js
+++ .eleventy.js
@@ -28,15 +28,16 @@ async function imageShortcode(src, alt) {
       return `${name}-${width}w.${format}`
     }
   })  
-  let lowsrc = metadata.jpeg[0]  
+  let lowsrc = metadata.jpeg[0]
+  let highsrc = metadata.jpeg[metadata.jpeg.length - 1]
   return `<picture>
     ${Object.values(metadata).map(imageFormat => {
       return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`
     }).join("\n")}
     <img
       src="${lowsrc.url}"
-      width="${lowsrc.width}"
-      height="${lowsrc.height}"
+      width="${highsrc.width}"
+      height="${highsrc.height}"
       alt="${alt}"
       loading="lazy"
       decoding="async">
